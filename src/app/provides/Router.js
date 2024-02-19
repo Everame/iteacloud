ccc1b3b1ee0a2fcc6a906afa7f6a69a6eb499574
ITea0fc <ilyatarasov@bk.ru>
2024-02-19 17:31:58 +0700

    remove basename

--- src/app/provides/Router.js
+++ src/app/provides/Router.js
@@ -4,24 +4,21 @@ import Homepage from '../../pages/Homepage/Homepage.jsx'
 import NotFound from '../../pages/404/404.jsx'
 import RequireAuth from './../../entities/RequireAuth/AuthRouter'
 
-export const Router = createBrowserRouter(
-    [
-        {
-            path: '/',
-            element: <RequireAuth element={Homepage} />,
-        },
-        {
-            path: '/auth',
-            element: <AuthPage />,
-        },
-        {
-            path: '/404',
-            element: <NotFound />,
-        },
-        {
-            path: '*',
-            element: <Navigate to="/404" replace />,
-        },
-    ],
-    { basename: 'helpful-khapse-cf0c1a.netlify.app' }
-)
+export const Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RequireAuth element={Homepage} />,
+    },
+    {
+        path: '/auth',
+        element: <AuthPage />,
+    },
+    {
+        path: '/404',
+        element: <NotFound />,
+    },
+    {
+        path: '*',
+        element: <Navigate to="/404" replace />,
+    },
+])
