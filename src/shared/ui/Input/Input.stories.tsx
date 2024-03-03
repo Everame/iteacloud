7bd6aa5454a1af648400e7365347aaa2e9cca1b6
ITea0fc <ilyatarasov@bk.ru>
2024-03-03 16:37:55 +0700

    Change render story input

--- src/shared/ui/Input/Input.stories.tsx
+++ src/shared/ui/Input/Input.stories.tsx
@@ -1,10 +1,14 @@
 import { Input } from './Input'
+import { Meta, Story } from '@storybook/react'
 
 export default {
     title: 'Input',
     component: Input,
-}
+} as Meta
 
-export const Default = () => (
-    <Input title="Площадь участка, га" placeholder="Например, 600" />
-)
+const Template: Story = (args: any) => <Input {...args} />
+export const Default = Template.bind({})
+Default.args = {
+    title: 'Площадь участка, га',
+    placeholder: 'Например, 600',
+}
